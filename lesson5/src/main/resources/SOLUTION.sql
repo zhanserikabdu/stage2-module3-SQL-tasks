1114b6ebe83a815a5a2830ba90472c2c15fcbf17
NANO <File changed>
2022-12-03 23:24:01 +0600

    2 submission

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -5,4 +5,4 @@ SELECT * FROM Student WHERE name LIKE 'Mike%' OR groupnumber IN (4, 5, 6);
 SELECT * FROM Payment WHERE DATEADD(MONTH, -8, CURRENT_DATE) < payment_date;
 SELECT * FROM Student WHERE name LIKE 'A%';
 SELECT * FROM Student WHERE (name LIKE 'Roxi%' AND groupnumber = 4) OR (name LIKE 'Tallie%' AND groupnumber = 9);
-Footer
+
